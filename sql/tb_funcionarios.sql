c66de5eaf683e21200e6fff9161b2b64c53231a2
joaonetto <joao.netto@actar.com.br>
2017-06-28 20:02:02 -0300

    tb_funcionarios -> Adicionando Data.

--- sql/tb_funcionarios.sql
+++ sql/tb_funcionarios.sql
@@ -11,6 +11,7 @@ create table actar.tb_funcionarios (
 	idFuncionario int auto_increment not null,
   nomeFuncionario varchar(256) not null,
   emailFuncionario varchar(256) not null,
+  dtFuncionarios datetime not null,
   constraint PK_Funcionario primary key(idFuncionario)
 
 ) engine = InnoDB DEFAULT CHARSET = UTF8 COLLATE = utf8_general_ci;
