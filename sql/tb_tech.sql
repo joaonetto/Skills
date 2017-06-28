05287bd7a65f5d5fb0fe9e7ab6f7f49825e3e1e2
joaonetto <joao.netto@actar.com.br>
2017-06-28 18:51:50 -0300

    tb_tech -> Retirada de relacionamento com tb_vendor

--- sql/tb_tech.sql
+++ sql/tb_tech.sql
@@ -11,15 +11,11 @@ create table actar.tb_tech (
       -> ISP
       -> Outros
 
-    A relacao externa se da com Quais Vendors tem as Tecnicas/Tecnologias acima
-    As perguntas tem que ter relacao com qual tecnologia ela pertence
+    Sem Relacao Externa
   */
 
 	idTech int auto_increment not null,
-  idVendor int not null,
   nomeTech varchar(256) not null,
-  constraint PK_Tech primary key(idTech),
-  constraint PK_Tech_Vendor foreign key(idVendor)
-		references actar.tb_vendor(idVendor)
+  constraint PK_Tech primary key(idTech)
 
 ) engine = InnoDB;
