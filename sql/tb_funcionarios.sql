1e0e279c98e30b5ec52586beafe9b4920e752b67
joaonetto <joao.netto@actar.com.br>
2017-06-29 08:52:03 -0300

    SQL -> Retirada Survey

--- sql/tb_funcionarios.sql
+++ sql/tb_funcionarios.sql
@@ -2,7 +2,6 @@ create table actar.tb_funcionarios (
 
   /*
     Tabela para o Cadastro dos Funcionarios.
-    Recebe relacao de idSurvey
 
     Auxilio sheets:
     ="insert into " & Tabelas!$B$1 & " values ( null, '" & tb_funcionarios!B2 & "', '" & tb_funcionarios!C2 & "');"
@@ -11,9 +10,6 @@ create table actar.tb_funcionarios (
 	idFuncionario int auto_increment not null,
   nomeFuncionario varchar(256) not null,
   emailFuncionario varchar(256) not null,
-  idSurvey int not null,
-  constraint PK_Funcionario primary key(idFuncionario),
-  constraint PK_Funcionario_Survey foreign key(idSurvey)
-    references actar.tb_survey(idSurvey)
+  constraint PK_Funcionario primary key(idFuncionario)
 
 ) engine = InnoDB DEFAULT CHARSET = UTF8 COLLATE = utf8_general_ci;
