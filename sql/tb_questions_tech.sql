c17d87b264798ca003c32d22f12be3f026778c0e
joaonetto <joao.netto@actar.com.br>
2017-07-04 09:18:35 -0300

    Incluindo SLIM

--- sql/tb_questions_tech.sql
+++ sql/tb_questions_tech.sql
@@ -10,12 +10,12 @@ create table actar.tb_questions_tech (
   idSurvey int not null,
   idQuestions int not null,
   idTech int not null,
-  constraint PK_Questions_Tech primary key(idQuestions_tech),
-  constraint PK_Questions_Tech_Survey foreign key(idSurvey)
+  constraint PK_QuestionsTech primary key(idQuestions_tech),
+  constraint PK_QuestionsTech_Survey foreign key(idSurvey)
     references actar.tb_survey(idSurvey),
-  constraint PK_Questions_Tech_Questions foreign key(idQuestions)
+  constraint PK_QuestionsTech_Questions foreign key(idQuestions)
     references actar.tb_questions(idQuestions),
-  constraint PK_Questions_Tech_Tech foreign key(idTech)
+  constraint PK_QuestionsTech_Tech foreign key(idTech)
     references actar.tb_tech(idTech)
 
 ) engine = InnoDB DEFAULT CHARSET = UTF8 COLLATE = utf8_general_ci;
