41648cb114e22c64ee9c091cd8e9b09ab264788b
joaonetto <joao.netto@actar.com.br>
2017-07-24 16:17:17 -0300

    Include jQuery and Bootstrap

--- view/header.php
+++ view/header.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="include/Bootstrap/css/bootstrap.min.css">
+		<script src="include/jQuery/jquery-3.2.1.min.js"></script>
+		<script src="include/Bootstrap/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="../css/skills.css">
 
 	</head>
--- view/index.php
+++ view/index.php
@@ -1,12 +1,44 @@
 <?php include_once("header.php");?>
-
+<?php
+  $sql = new Sql();
+  $data = $sql->select("SELECT * FROM actar.tb_funcionarios order by nomeFuncionario asc;");
+  //var_dump($data);
+?>
 <section>
-
-	<div id="corpo" class "corpo">
+	<div id="corpo">
     <h1>Estudo de Análise de Skills</h1>
-
+    <br>
+    <h5>
+      Neste primeiro estudo de Skills, a <strong>Área de Tecnologia</strong> levou em consideração para análise itens como:
+      <br>
+      <br>
+      <ul>
+        <li>Fabricantes;</li>
+        <li>Tecnologias;</li>
+        <li>Áreas de Interesse;</li>
+        <li>Ranking dos profissionais.</li>
+      </ul>
+      <br>
+      <br>
+      A lista abaixo demonstra quais profissionais estão contidos neste estudo, veja:
+    </h5>
+    <br>
+    <table style="width:60%">
+      <tr>
+        <th>Nome</th>
+        <th>E-mail</th>
+      </tr>
+      <?php
+        foreach ($data as $column) {
+          echo '<tr>';
+          echo '<td>' . $column['nomeFuncionario'];
+          echo '<td><a href=mailto:' . $column['emailFuncionario'] . '>'. $column['emailFuncionario'] . '</a></td>';
+          echo '</tr>';
+        }
+        unset($data, $column, $sql);
+      ?>
+    </table>
 	</div>
-
 </section>
 </body>
 </html>
