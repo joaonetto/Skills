8a4b96eb1e090a83ce074e386d560709aea91b26
joaonetto <joao.netto@actar.com.br>
2017-07-29 08:15:11 -0300

    Ajustes Header e Configuration Adicionando Variaveis Globais

--- include/configuration.php
+++ include/configuration.php
@@ -1,27 +1,51 @@
 <?php
-session_start();
-class Sql {
-	public $conn;
-	public function __construct(){
-		return $this->conn = mysqli_connect("10.0.1.10", "root", "Laboratorio", "actar");
+	session_start();
+	class Sql {
+		public $conn;
+		public function __construct(){
+			return $this->conn = mysqli_connect("10.0.1.10", "root", "Laboratorio", "actar");
+		}
+		public function query($string_query){
+			return mysqli_query($this->conn, $string_query);
+		}
+		public function select($string_query){
+			$result = $this->query($string_query);
+			$data = array();
+		    while ($row = mysqli_fetch_array($result)) {
+		    	foreach ($row as $key => $value) {
+		    		$row[$key] = utf8_encode($value);
+		    	}
+		        array_push($data, $row);
+		    }
+		    unset($result);
+		    return $data;
+		}
+		public function __destruct(){
+			mysqli_close($this->conn);
+		}
 	}
-	public function query($string_query){
-		return mysqli_query($this->conn, $string_query);
-	}
-	public function select($string_query){
-		$result = $this->query($string_query);
-		$data = array();
-	    while ($row = mysqli_fetch_array($result)) {
-	    	foreach ($row as $key => $value) {
-	    		$row[$key] = utf8_encode($value);
-	    	}
-	        array_push($data, $row);
-	    }
-	    unset($result);
-	    return $data;
-	}
-	public function __destruct(){
-		mysqli_close($this->conn);
-	}
-}
+
+	$sql = new Sql();
+	$result1 = $sql->select("
+		SELECT
+			COUNT(actar.tb_vendors.idVendor)
+		FROM
+			actar.tb_vendors;");
+	$qtd_Vendors = intval($result1[0][0]);
+
+	$result1 = $sql->select("
+		SELECT
+			COUNT( actar.tb_departamentos.idDepartamento )
+		FROM
+			actar.tb_departamentos;");
+	$qtd_Departamento = intval($result1[0][0]);
+
+	$result1 = $sql->select("
+		SELECT
+			COUNT( actar.tb_tech_area.idTech_Area )
+		FROM
+			actar.tb_tech_area;");
+	$qtd_TechArea = intval($result1[0][0]);
+
+	unset($result1, $sql);
 ?>
--- view/header.php
+++ view/header.php
@@ -1,3 +1,20 @@
+<?php
+	$sql = new Sql();
+	$result1 = $sql->select("
+		SELECT
+			actar.tb_vendors.idVendor, actar.tb_vendors.nomeVendor
+		FROM
+			actar.tb_vendors
+		ORDER BY
+			actar.tb_vendors.nomeVendor ASC;");
+	$result2 = $sql->select("
+		SELECT
+			actar.tb_tech_area.idTech_Area, actar.tb_tech_area.nomeTech_Area
+		FROM
+			actar.tb_tech_area
+		ORDER BY
+			actar.tb_tech_area.nomeTech_Area ASC;");
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -25,23 +42,25 @@
           <div class="btn-group">
               <button type="button" data-toggle="dropdown" class="btn btn-sm btn-primary dropdown-toggle">Questões por Fabricantes<span class="caret"></span></button>
               <ul class="dropdown-menu">
-                  <li><a href="vendors-1">Checkpoint</a></li>
-                  <li><a href="vendors-2">Cisco</a></li>
-                  <li><a href="vendors-3">Juniper</a></li>
-                  <li><a href="vendors-4">Pulse Secure</a></li>
-									<li><a href="vendors-5">Radware</a></li>
-                  <li><a href="vendors-6">Symantec</a></li>
+								<?php
+								foreach ($result1 as $column) {
+									echo '<li><a href="vendors-'. $column['idVendor'] .'">' . $column['nomeVendor'] . '</a></li>';
+								}
+								?>
                   <li class="divider"></li>
                   <li><a href="vendors-0">Resumo Fabricantes</a></li>
               </ul>
           </div>
           <div class="btn-group">
-              <button type="button" data-toggle="dropdown" class="btn btn-sm btn-primary dropdown-toggle">Action <span class="caret"></span></button>
-              <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
+              <button type="button" data-toggle="dropdown" class="btn btn-sm btn-primary dropdown-toggle">Questões por Tecnologia <span class="caret"></span></button>
+              <ul class="dropdown-menu dropdown-menu-right">
+								<?php
+								foreach ($result2 as $column) {
+									echo '<li><a href="tech-'. $column['idTech_Area'] .'">' . $column['nomeTech_Area'] . '</a></li>';
+								}
+								?>
                   <li class="divider"></li>
-                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">Resumo Tecnologia</a></li>
               </ul>
           </div>
           <div class="btn-group">
