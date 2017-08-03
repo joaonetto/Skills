36e2e7489f0cb8028041bb5056e872b8fabb21a3
joaonetto <joao.netto@actar.com.br>
2017-08-03 10:55:46 -0300

    Ajuste Final de Menu

--- view/header.php
+++ view/header.php
@@ -43,57 +43,13 @@
       <div id="frasetopo" class="header-black">
         <h1>Análise de Skills ACTAR</h1>
       </div>
-			<div class="row header-teste">
+			<div class="row header-tabela">
 				<div class="col-md-12 header-posicao-menu">
 					<div class="btn-group">
 					  <button type="button" class="btn btn-success">Inicio</button>
 						<div class="btn-group">
 							<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Questões por Fabricantes<span class="caret"></span></button>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="#">Tablet</a></li>
-								<li><a href="#">Smartphone</a></li>
-							</ul>
-						</div>
-						<div class="btn-group">
-							<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Questões por Tecnologia<span class="caret"></span></button>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="#">Tablet</a></li>
-								<li><a href="#">Smartphone</a></li>
-							</ul>
-						</div>
-						<div class="btn-group">
-							<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Ferramentas ACTAR<span class="caret"></span></button>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="#">Tablet</a></li>
-								<li><a href="#">Smartphone</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="row header-teste">
-				<div class="col-md-12 header-jnetto">
-					Powered by ACTAR Technologies. All rights reserved.<br>Created by JNetto</p>
-				</div>
-			</div>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			tb_questions_ferramentas_answers
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-
-      <div class="header-topo">
-        <div class="header-menu">
-          <a class="btn btn-sm btn-success" href="index.php" role="button">Inicio</a>
-          <div class="btn-group">
-              <button type="button" data-toggle="dropdown" class="btn btn-sm btn-primary dropdown-toggle">Questões por Fabricantes<span class="caret"></span></button>
-              <ul class="dropdown-menu">
+							<ul class="dropdown-menu">
 								<?php
 								foreach ($result1 as $column) {
 									echo '<li><a href="vendors-'. $column['idVendor'] .'">' . $column['nomeVendor'] . '</a></li>';
@@ -102,10 +58,10 @@
                   <li class="divider"></li>
                   <li><a href="vendors-0">Resumo Fabricantes</a></li>
               </ul>
-          </div>
-          <div class="btn-group">
-              <button type="button" data-toggle="dropdown" class="btn btn-sm btn-primary dropdown-toggle">Questões por Tecnologia <span class="caret"></span></button>
-              <ul class="dropdown-menu dropdown-menu-right">
+						</div>
+						<div class="btn-group">
+							<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Questões por Tecnologia<span class="caret"></span></button>
+							<ul class="dropdown-menu dropdown-menu-right">
 								<?php
 									foreach ($result2 as $column) {
 										echo '<li><a href="tech-'. $column['idTech_Area'] .'">' . $column['nomeTech_Area'] . '</a></li>';
@@ -114,10 +70,10 @@
                   <li class="divider"></li>
                   <li><a href="tech-0">Resumo Tecnologia</a></li>
               </ul>
-          </div>
-          <div class="btn-group">
-              <button type="button" data-toggle="dropdown" class="btn btn-sm btn-warning dropdown-toggle">Ferramentas ACTAR<span class="caret"></span></button>
-              <ul class="dropdown-menu">
+						</div>
+						<div class="btn-group">
+							<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">Ferramentas ACTAR<span class="caret"></span></button>
+							<ul class="dropdown-menu">
 								<?php
 									foreach ($result3 as $column) {
 										echo '<li><a href="ferramentas-'. $column['idFerramenta'] .'">' . $column['nomeFerramenta'] . '</a></li>';
@@ -126,30 +82,13 @@
                   <li class="divider"></li>
                   <li><a href="#">Resumo Ferramentas</a></li>
               </ul>
-          </div>
-          <div class="btn-group">
-              <button type="button" data-toggle="dropdown" class="btn btn-sm btn-warning dropdown-toggle">Action <span class="caret"></span></button>
-              <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li class="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-              </ul>
-          </div>
-          <div class="btn-group">
-              <button type="button" data-toggle="dropdown" class="btn btn-sm btn-danger dropdown-toggle">Action <span class="caret"></span></button>
-              <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li class="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-              </ul>
-          </div>
-        </div>
-				<div class="header-copyright">
-					<div class="copyright">
-						<p class="pull-right text-roxo">Powered by ACTAR Technologies. All rights reserved.<br>Created by JNetto</p>
+						</div>
 					</div>
+				</div>
+			</div>
+			<div class="row header-copyright">
+				<div class="col-md-12 header-author">
+					Powered by ACTAR Technologies. All rights reserved.<br>Created by JNetto</p>
 				</div>
       </div>
 		</header>
