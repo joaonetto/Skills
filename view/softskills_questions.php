39a1d1cb4728e53d07ce9fcc6211b42cf4026518
joaonetto <joao.netto@actar.com.br>
2017-08-03 21:37:00 -0300

    Iniciando SoftSkills Questions

--- view/softskills_questions.php
+++ view/softskills_questions.php
@@ -23,3 +23,75 @@
     ORDER BY
       actar.tb_questions.desQuestions ASC;');
 ?>
+<section>
+  <div id="corpo">
+    <h1>Questões Destinadas ao Soft Skill ACTAR: <?php echo $result1 ?></h1>
+    <br>
+    <h5>
+      Para cada área de interesse da pesquisa, foi informado aos participantes que pontuassem seguindo a regra abaixo, veja:
+    </h5>
+    <h3>
+        Soft Skills ACTAR
+    </h3>
+    <h5>
+      1 - Nãor tem tais qualificações<br>
+      2 - Sim. Qualificações Parciais;<br>
+      3 - Sim. Tem completamente as Qualificações;<br>
+    </h5>
+  </div>
+  <br>
+  <br>
+  <?php
+    $Count1 = 1;
+    $Count2 = 1;
+    $myAnswers_Tech = Array();
+    foreach ($result2 as $column) {
+      for ($Count1 = 1; $Count1 <= 5; $Count1++){
+        $result3 = $sql->select('
+          SELECT
+            COUNT( actar.tb_questions_ferramentas_answers.answerFuncFerramenta )
+          FROM ( actar.tb_questions_ferramentas_answers
+            INNER JOIN actar.tb_funcionarios	on actar.tb_funcionarios.idFuncionario		= actar.tb_questions_ferramentas_answers.idFuncionario)
+          WHERE
+                ( actar.tb_questions_ferramentas_answers.idSurvey             = ' . intval($GLOBALS['$analiseSurvey']) . ' )
+            and ( actar.tb_funcionarios.analiseFuncionario                    = 1 )
+            and ( actar.tb_questions_ferramentas_answers.idFerramenta         = ' . $id_func . ' )
+            and ( actar.tb_questions_ferramentas_answers.idQuestions          = ' . $column['idQuestions'] . ' )
+            and ( actar.tb_questions_ferramentas_answers.answerFuncFerramenta = ' . $Count1 . ' );');
+        $myAnswers_Tech[$Count1] = $result3[0][0];
+      }
+      echo '<div class="panel panel-default">';
+      echo '	<div class="panel-heading"><a href="ferrquestion-' . $column['idQuestions'] . '-(' . implode(", ",$myAnswers_Tech) . ')">' . $column['desQuestions'] . '</a></div>';
+      echo '	<div class="panel-body">';
+      echo '	 <div class="panel-espaco-interno">';
+      echo '    <div class="row">';
+      echo '      <div class="col-md-4 cabecalho-vazio"></div>';
+      echo '      <div class="col-md-4 cabecalho-tabela">Gráfico</div>';
+      echo '      <div class="col-md-4 cabecalho-vazio"></div>';
+      echo '    </div>';
+      echo '    <div class="row">';
+      echo '      <div class="col-md-4 cabecalho-vazio"></div>';
+      echo '      <div id="graphTipoA' . $Count2++ . '" ChartValues="'
+                          . implode(", ",$myAnswers_Tech) . '"></div>';
+      echo '      <div class="col-md-4 cabecalho-vazio"></div>';
+      echo '    </div>';
+      echo '   </div>';
+      echo '  </div>';
+      echo '</div>';
+    }
+    unset($result1, $result2, $total_result1, $total_result2, $column, $sql, $myAnswers_Pre, $myAnswers_Del, $myAnswers_Sup);
+
+  ?>
+  <br>
+  <br>
+  </div>
+</section>
+</body>
+</html>
+<script src="../include/js/ChartA.js"></script>
+<?php
+  $time_end = microtime(true);
+
+  $time = $time_end - $time_start;
+  echo 'Processado e Carregado em: ' . $time;
+?>
